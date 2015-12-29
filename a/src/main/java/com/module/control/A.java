eac642aaba83c7731144d8621d1555e20a00c1b2
yhyuan <yhyuan@shendu.com>
2015-12-29 18:36:12 +0800

    spring configuration

--- a/src/main/java/com/module/control/A.java
+++ a/src/main/java/com/module/control/A.java
@@ -5,17 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import ch.qos.logback.ext.spring.ApplicationContextHolder;
-
 public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "services.xml"});
-		Logger lg =  (Logger) LoggerFactory.getLogger("A");
-		lg.debug("aAAA");
+		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "spring/ApplicationContext.xml"});
+		Logger lg =  (Logger) LoggerFactory.getLogger(A.class);
+		//lg.debug("aAAA");
 		// retrieve configured instance
-		ApplicationContextHolder log = (ApplicationContextHolder) context.getBean("log");
+		Logger log = (Logger) context.getBean("log");
+		log.debug("TEEE");
+		
 	}
 
 }
