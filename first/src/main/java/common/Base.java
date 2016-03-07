ce74c3fbf2fdd24d1e38323a255defea4cce6e99
yhyuan <yhyuan@shendu.com>
2016-03-07 16:10:57 +0800

    新建项目

---
+++ first/src/main/java/common/Base.java
@@ -0,0 +1,8 @@
+package common;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public interface Base {
+	public static Log log = LogFactory.getLog(Base.class);
+}
