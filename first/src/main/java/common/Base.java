e31296ea31299ebcaa3eb27ea30830208b682ee1
yhyuan <yhyuan@shendu.com>
2016-03-07 16:32:48 +0800

    rm

--- first/src/main/java/common/Base.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-public interface Base {
-	public static Log log = LogFactory.getLog(Base.class);
-}
