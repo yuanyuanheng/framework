ba9861420b26e480a519d082905de3fa15ee6792
yhyuan <yhyuan@shendu.com>
2016-03-28 18:32:04 +0800

    添加springmvc

---
+++ spring_mybatis_mysql/src/main/java/com/module/Index.java
@@ -0,0 +1,18 @@
+package com.module;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/")
+public class Index {
+
+	@RequestMapping("index")
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index");
+		return mv;
+	}
+
+}
